784fc134143fbbc8db2aae207aab6fe16e715bed
unknown <diegoalparra08@gmail.com>
2026-07-10 11:52:26 -0500

    feat: added 15 days verification to rate again player in Rating controller Post

--- FutbolStatsWithFriends/DTOs/Rating/RatingCreateDTO.cs
+++ FutbolStatsWithFriends/DTOs/Rating/RatingCreateDTO.cs
@@ -5,7 +5,6 @@ namespace FutbolStatsWithFriends.DTOs.Rating
     public class RatingCreateDTO : RatingUpdateDTO
     {
         // Las estadisticas las tomamos del Update
-        public int UserId { get; set; }
         public int PlayerId { get; set; }
     }
 }
